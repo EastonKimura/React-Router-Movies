52851ed18c30c3aa509b326cea9df8e20e6f18ff
unknown <eastkim1@hotmail.com>
2020-01-29 10:19:55 -0700

    MVP done

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,8 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList}/>
-<Route path="/movies/:id" component={Movie}/>
+      <Route path="/movies/:id" render={ (props)=> <Movie item={MovieList} {...props}/>}/>
+{}
 {}
     </div>
   );
